f3fa69c5de6e26ed9d11a07f42ac2a1f4f4af330
Ibrahima DANSOKO <ibrahima.Dansoko@outlook.com>
2020-10-07 18:41:17 +0200

    add route weather

--- src/functions/weather.ts
+++ src/functions/weather.ts
@@ -1,8 +1,20 @@
-export  async function WeatherFunction (_event, _context) {
-    return {
-        statusCode: 200,
-        body: JSON.stringify({
-            message: 'meteo',
-        }, null, 2),
-    };
+import axios from 'axios';
+
+export async function WeatherFunction(_event, _context) {
+    return await axios.get(`http://api.openweathermap.org/data/2.5/weather?q=Paris&appid=${process.env.WEATHER_API_KEY as string}`).then(({ data }) => {
+        return {
+            statusCode: 200,
+            body: JSON.stringify({
+                message: data,
+            }, null, 2),
+        };
+    }).catch(err => {
+        return {
+            statusCode: err.status,
+            body: JSON.stringify({
+                message: err.message,
+            }, null, 2),
+        };
+    });
+
 }
--- src/handler.ts
+++ src/handler.ts
@@ -1,7 +1,9 @@
 import 'source-map-support/register';
 import { Handler } from 'aws-lambda';
+import {config} from 'dotenv';
 import { HelloFunction, WeatherFunction } from './functions';
 
+config()
 
 export const hello: Handler = HelloFunction;
 export const weather: Handler = WeatherFunction;
